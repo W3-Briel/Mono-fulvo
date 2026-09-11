8e8f8ff14c511c8c1b8bcfa01a12f80d49bed6e7
Angel Saucedo <100446374+W3-Briel@users.noreply.github.com>
2026-09-11 15:44:02 +0000

    modelando propiedad reflexiva en pozo

--- backend/app/models/pozo.ts
+++ backend/app/models/pozo.ts
@@ -4,8 +4,18 @@ import Partido from './partido.ts'
 import type { BelongsTo } from '@adonisjs/lucid/types/relations'
 
 export default class Pozo extends PozoSchema {
-    @belongsTo(()=> Partido, {
+    @belongsTo(() => Partido, {
         foreignKey: "partidoId"
     })
     declare partido: BelongsTo<typeof Partido>
+
+    @belongsTo(() => Pozo, {
+        foreignKey: "pozoPadre"
+    })
+    declare padre: BelongsTo<typeof Pozo>
+
+    @hasMany(() => Pozo, {
+        foreignKey: 'pozoPadre',
+    })
+    declare hijos: HasMany<typeof Pozo>
 }
