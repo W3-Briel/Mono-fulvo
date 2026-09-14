ee650c6d90fbd40947462afbcb79c6ce3c23947f
Angel Saucedo <100446374+W3-Briel@users.noreply.github.com>
2026-09-14 16:12:31 +0000

    arreglando importacion y cambiando nombre de una variable

--- backend/app/models/pozo.ts
+++ backend/app/models/pozo.ts
@@ -1,7 +1,7 @@
 import { PozoSchema } from '#database/schema'
-import { belongsTo } from '@adonisjs/lucid/orm'
+import { belongsTo, hasMany } from '@adonisjs/lucid/orm'
 import Partido from './partido.ts'
-import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import type { BelongsTo, HasMany } from '@adonisjs/lucid/types/relations'
 
 export default class Pozo extends PozoSchema {
     @belongsTo(() => Partido, {
